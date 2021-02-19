4af33d60c46b0e4222a9f1a64f228b5d772643bf
ilumin <iolumin@gmail.com>
2021-02-19 15:14:29 +0700

    add: content

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,40 @@
 module.exports = {
   siteMetadata: {
-    title: 'gatsby-starter-code-notes',
-    description: `A starter for gatsby-theme-code-notes`,
-    author: 'Zander',
+    title: "ilumin.dev",
+    description: `@ilumin notes`,
+    author: "@ilumin",
   },
   plugins: [
     {
-      resolve: 'gatsby-theme-code-notes',
+      resolve: "gatsby-theme-code-notes",
       options: {
-        contentPath: 'notes',
-        basePath: '/',
+        contentPath: "notes",
+        basePath: "/",
+        // logo: `static/favicon/logo.png`,
+        showDate: true,
         showThemeInfo: true,
         showDescriptionInSidebar: true,
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `@ilumin Notes`,
+        short_name: `ilumin.dev`,
+        description: `@ilumin memory bank`,
+        start_url: `/`,
+        background_color: `hsl(210, 38%, 95%)`,
+        theme_color: `hsl(345, 100%, 69%)`,
+        display: `standalone`,
+        icon: `static/favicon/logo.png`,
+        showDate: true,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-offline`,
+      options: {
+        precachePages: [`/*`, `/tag/*`],
+      },
+    },
   ],
-}
+};
